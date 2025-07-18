cd43ab033932db4793c7b30324b353bee9dd9f89
dirkules41 <max.liebler.psn@gmail.com>
2025-07-18 23:43:09 +0200

    Updated Animation for Aura Sphere

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -4599,7 +4599,7 @@ BattleAnim_BeatUp:
 
 BattleAnim_AuraSphere:
 	anim_1gfx ANIM_GFX_OBJECTS
-	anim_bgeffect ANIM_BG_CYCLE_OBPALS_BLUE_GREEN, $0, $3, $0
+	anim_bgeffect ANIM_BG_CYCLE_OBPALS_GRAY_AND_YELLOW, $0, $3, $0
 	anim_bgeffect ANIM_BG_CYCLE_BGPALS_INVERTED, $0, $4, $0
 	anim_sound 6, 2, SFX_METRONOME
 	anim_obj ANIM_OBJ_SWIFT, 64, 88, $4
@@ -4607,10 +4607,11 @@ BattleAnim_AuraSphere:
     	anim_obj ANIM_OBJ_SWIFT, 64, 80, $4
     	anim_wait 2
    	anim_obj ANIM_OBJ_SWIFT, 64, 72, $4
- 	anim_obj ANIM_OBJ_SPARKLE, 60, 78, $2
+ 	anim_obj ANIM_OBJ_SHOOTING_SPARKLE, 60, 78, $2
     	anim_wait 1
-    	anim_obj ANIM_OBJ_SPARKLE, 68, 82, $2
-	anim_wait 64
+    	anim_obj ANIM_OBJ_SHOOTING_SPARKLE, 68, 82, $2
+	anim_wait 1
+	anim_wait 32
 	anim_ret
 
 BattleAnimSub_Drain:
