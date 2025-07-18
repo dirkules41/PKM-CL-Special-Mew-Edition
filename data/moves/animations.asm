7da150471b47701a01ad2a30123d6b085bdbfe83
dirkules41 <max.liebler.psn@gmail.com>
2025-07-18 23:05:04 +0200

    AuraSphere updated to mix of Swift and Psybeam

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -4598,15 +4598,17 @@ BattleAnim_BeatUp:
 	anim_ret
 
 BattleAnim_AuraSphere:
-    	anim_1gfx ANIM_GFX_OBJECTS
-    	anim_sound 6, 2, SFX_METRONOME
-    	anim_obj ANIM_OBJ_SWIFT, 64, 88, $4
-    	anim_wait 4
-    	anim_obj ANIM_OBJ_SWIFT, 64, 72, $4
-    	anim_wait 4
-    	anim_obj ANIM_OBJ_SWIFT, 64, 76, $4
-    	anim_wait 64
-    	anim_ret
+	anim_1gfx ANIM_GFX_OBJECTS
+	anim_bgeffect ANIM_BG_CYCLE_OBPALS_GRAY_AND_YELLOW, $0, $2, $0
+	anim_bgeffect ANIM_BG_CYCLE_BGPALS_INVERTED, $0, $4, $0
+	anim_sound 6, 2, SFX_METRONOME
+	anim_obj ANIM_OBJ_SWIFT, 64, 88, $4
+	anim_wait 4
+	anim_obj ANIM_OBJ_SWIFT, 64, 72, $4
+	anim_wait 4
+	anim_obj ANIM_OBJ_SWIFT, 64, 76, $4
+	anim_wait 64
+	anim_ret
 
 BattleAnimSub_Drain:
 	anim_obj ANIM_OBJ_DRAIN, 132, 44, $0
